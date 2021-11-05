b185f2cb6c3d728975d66df278c5f46c15875430
Fabian Reyna <3753836700101@ingenieria.usac.edu.gt>
2021-11-05 12:17:49 -0600

    final

--- proyectoMC2.py
+++ proyectoMC2.py
@@ -4,7 +4,8 @@ from analizadorLexico import *
 from tkinter import simpledialog
 from analizadorSintactico import analizadorSintactico
 from pila import pila
-import webbrowser,os
+import re
+from tkinter import messagebox
 
 scanner = AnalizadorLexico()
 root=Tk()
@@ -22,6 +23,9 @@ operador2=Text(root,wrap=NONE)
 
 numerador3=Text(root,wrap=NONE)
 denominador3=Text(root,wrap=NONE)
+lb_not_pol=Label(root,text='',background='skyblue')
+lb_res=Label(root,text='',background='skyblue')
+
 
 
 def main():
@@ -48,6 +52,8 @@ def main():
     Label(root,text='--------------------------------------------------',background='skyblue').place(x=650,y=85)
     denominador3.place(x=650,y=110,height=30,width=250)
     Button(root,text='Operar',command=operar).place(x=410,y=175,width=100,height=30)
+    lb_not_pol.place(x=350,y=225)
+    lb_res.place(x=350,y=260)
     root.mainloop()
 
 
@@ -131,7 +137,97 @@ def operar():
     while aux!=None:
         cadenaPrueba=aux.elemento+cadenaPrueba
         aux=aux.ant
+    cadenaPrueba=cadenaPrueba.replace('^','↑')
+    lb_not_pol.config(text='NOTACION POLACA ' +cadenaPrueba)
+
     print(cadenaPrueba)
+
+    for key in diccionario:
+        cadenaPrueba=cadenaPrueba.replace(key,diccionario[key])
+    
+    div=re.search('\/(\[(\d+(\.\d)?)|([A-Z]))\]\[((\d+(\.\d)?)|([A-Z]))\]',cadenaPrueba)
+    pro=re.search('\*(\[(\d+(\.\d)?)|([A-Z]))\]\[((\d+(\.\d)?)|([A-Z]))\]',cadenaPrueba)
+    pot=re.search('\↑(\[(\d+(\.\d)?)|([A-Z]))\]\[((\d+(\.\d)?)|([A-Z]))\]',cadenaPrueba)
+    sum=re.search('\+(\[(\d+(\.\d)?)|([A-Z]))\]\[((\d+(\.\d)?)|([A-Z]))\]',cadenaPrueba)
+    res=re.search('\-(\[(\d+(\.\d)?)|([A-Z]))\]\[((\d+(\.\d)?)|([A-Z]))\]',cadenaPrueba)
+
+    while div!= None or pro!=None or pot!=None or sum!=None or res!=None:
+
+        if div!=None:
+            separa=re.split('\[',div[0])
+            separa[1]=separa[1].replace(']','')
+            separa[2]=separa[2].replace(']','')
+            
+
+            separa[1]=float(separa[1])
+            
+            separa[2]=float(separa[2])
+
+            operar=separa[1]/separa[2]
+            cadenaPrueba=cadenaPrueba.replace(div[0],'['+str(operar)+']')
+        
+        if pro!=None:
+            separa=re.split('\[',pro[0])
+            separa[1]=separa[1].replace(']','')
+            separa[2]=separa[2].replace(']','')
+            
+
+            separa[1]=float(separa[1])
+            
+            separa[2]=float(separa[2])
+
+            operar=separa[1]*separa[2]
+            cadenaPrueba=cadenaPrueba.replace(pro[0],'['+str(operar)+']')
+
+        if pot!=None:
+            separa=re.split('\[',pot[0])
+            separa[1]=separa[1].replace(']','')
+            separa[2]=separa[2].replace(']','')
+            
+
+            separa[1]=float(separa[1])
+            
+            separa[2]=float(separa[2])
+
+            operar=separa[1]**separa[2]
+            cadenaPrueba=cadenaPrueba.replace(pot[0],'['+str(operar)+']')
+    
+        if sum!=None:
+            separa=re.split('\[',sum[0])
+            separa[1]=separa[1].replace(']','')
+            separa[2]=separa[2].replace(']','')
+        
+            separa[1]=float(separa[1])
+            separa[2]=float(separa[2])
+
+            operar=separa[1]+separa[2]
+            cadenaPrueba=cadenaPrueba.replace(sum[0],'['+str(operar)+']')
+
+        if res!=None:
+            separa=re.split('\[',res[0])
+            separa[1]=separa[1].replace(']','')
+            separa[2]=separa[2].replace(']','')
+            
+
+            separa[1]=float(separa[1])
+            separa[2]=float(separa[2])
+
+            operar=separa[1]-separa[2]
+            cadenaPrueba=cadenaPrueba.replace(res[0],'['+str(operar)+']')
+
+        div=re.search('\/(\[\d+(\.\d)?|[A-Z])\]\[(\d+(\.\d)?|[A-Z])\]',cadenaPrueba)
+        pro=re.search('\*(\[\d+(\.\d)?|[A-Z])\]\[(\d+(\.\d)?|[A-Z])\]',cadenaPrueba)
+        pot=re.search('\↑(\[\d+(\.\d)?|[A-Z])\]\[(\d+(\.\d)?|[A-Z])\]',cadenaPrueba)
+        sum=re.search('\+(\[\d+(\.\d)?|[A-Z])\]\[(\d+(\.\d)?|[A-Z])\]',cadenaPrueba)
+        res=re.search('\-(\[\d+(\.\d)?|[A-Z])\]\[(\d+(\.\d)?|[A-Z])\]',cadenaPrueba)
+    
+
+    lb_res.config(text='RESPUESTA '+cadenaPrueba)
+        
+
+    
+
+
 
 
     # for a in copiaLista:
